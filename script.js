ac81c222a3decf0ed2c0de7e9edc655976347ba2
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-09-22 00:13:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const levelElement = document.getElementBy("level");
+let level = 0;
+ while(true){
+	 if(levelElement.tagName === "HTML"){
+		 break;
+	 }
+	 levelElement = levelElement.parentNode;
+	 level++;
+ }
+
+alert(`The level of the element is: ${level}`);
